af1d832870c03f08292715a9bf6f3631930a60a8
Paulo Becker <beckerpaulo81@gmail.com>
2026-05-09 20:16:14 -0300

    Criando a função .find em JS

---
+++ src/exe-find.js
@@ -0,0 +1,14 @@
+// O QUE O .find FAZ
+// Vou te retornar o primeiro valor que atenda a condição. 
+function nossoFind(lista, funcaoCondicao) {
+    for (let index = 0; index < lista.length; index++) {
+        const element = lista[index];
+        if (funcaoCondicao(element)) {
+            return element;
+        }
+    }
+    return undefined;
+}
+
+console.log(nossoFind(["banana", "maçã", "uva", "abacaxi", "laranja", "manga", "maracujá"], x => x.length > 6));
+console.log(nossoFind([1, 81, 3, 4, 99, 6, 45], x => x < 4));
